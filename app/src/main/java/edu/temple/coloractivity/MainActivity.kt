f5bc7d798116edae4f0013af0ed05899188f4f56
ArtlessBoi <jayanlas00@gmail.com>
2022-02-13 22:15:54 -0500

    I think this is kinda how it is supposed to look?

--- app/src/main/java/edu/temple/coloractivity/MainActivity.kt
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.kt
@@ -1,13 +1,25 @@
 package edu.temple.coloractivity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        val colors = arrayOf("Red", "Green", "Blue", "Yellow", "Cyan", "Magenta", "Gray", "Black")
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val listView = findViewById<ListView>(R.id.listView)
+
+        val adapter = ColorAdapter(this, colors)
+
+        spinner.adapter = adapter
+        listView.adapter = adapter
+
+
     }
 }
